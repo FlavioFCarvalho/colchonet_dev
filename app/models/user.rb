30680e94834136e531b1519331b2f7d6b035292f
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-05-23 14:30:49 -0300

    Criando um expressão regular para validar o e-mail e ativando o unique nele
    Signed-off-by: Flavio Carvalho <flavio@globalnetsis.com.br>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	validates_presence_of :email, :full_name, :location, :password
-
 	validates_confirmation_of :password
 	validates_length_of :bio, in: 5..10, allow_blank: false
+
+	validates_uniqueness_of :email
+
+	private 
+
+	# Essa validação pode ser representada da seguinte forma:
+    # validates_format_of :email, with: EMAIL_REGEXP
+
+    def email_format
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
+
 end
