38d30409ffc23c27744f5be8811602fc9fb13002
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-05-23 11:31:58 -0300

    Validando o campo "bio" in: 5..10 para validar o mínimo de  e o máximo de  caracteres;  eo allow_blank : Permite que o atributo que em branco, e, quando presente, respeite as condições de tamanho;
    Signed-off-by: Flavio Carvalho <flavio@globalnetsis.com.br>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	validates_presence_of :email, :full_name, :location, :password
 
 	validates_confirmation_of :password
+	validates_length_of :bio, in: 5..10, allow_blank: false
 end
