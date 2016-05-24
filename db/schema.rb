245d6e2a66e6ad94db491e8ef773eb0cdd14a249
FlavioFCarvalho <flaviocfr@gmail.com>
2016-05-23 21:30:28 -0300

    Removendo a coluna password em User
    Signed-off-by: FlavioFCarvalho <flaviocfr@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523235836) do
+ActiveRecord::Schema.define(version: 20160524002449) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160523235836) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name",       limit: 255
     t.string   "email",           limit: 255
-    t.string   "password",        limit: 255
     t.string   "location",        limit: 255
     t.text     "bio",             limit: 65535
     t.datetime "created_at"
