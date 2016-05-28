4a0a73f40d5878e1075e4a91d454aa7eaaba5165
FlavioFCarvalho <flaviocfr@gmail.com>
2016-05-28 11:38:00 -0300

    Enviando a confirmação do cadastro em user, cadastrando a hora da confirmação e removendo o token do banco
    Signed-off-by: FlavioFCarvalho <flaviocfr@gmail.com>

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -2,9 +2,10 @@ class SignupMailer < ActionMailer::Base
 	default from: 'no-reply@colcho.net'
 	def confirm_email(user)
 		@user = user
-		# Link temporário pois a funcionalidade ainda
-		# não existe, vamos criar ainda neste capítulo
-		@confirmation_link = root_url
+		
+		@confirmation_link = confirmation_url({
+			token: @user.confirmation_token
+		})
 		mail({
 			to: user.email,
 			bcc: ['sign ups <signups@colcho.net>'],
