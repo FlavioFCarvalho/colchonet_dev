dad164baa245905c36750362a1e73c9b837c0407
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-05-31 11:16:15 -0300

    Delegando responsabilidades ao user_session - Controle de acesso
    Signed-off-by: Flavio Carvalho <flavio@globalnetsis.com.br>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  delegate :current_user, :user_signed_in?, to: :user_session
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_session
+	UserSession.new(session)
+  end
 end
