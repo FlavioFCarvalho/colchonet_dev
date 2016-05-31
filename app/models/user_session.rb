4076e64b726335ba45156f9e6dfe15735d0ac1d4
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-05-31 11:09:54 -0300

    Criando os métodos para busca o usuário na sessão e verifica se a sessão é válida no UserSession - Controle de acesso
    Signed-off-by: Flavio Carvalho <flavio@globalnetsis.com.br>

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -24,5 +24,13 @@ class UserSession
 	def store(user)
 		@session[:user_id] = user.id
 	end
+    #Busca o usuário ma sessão 
+	def current_user
+		User.find(@session[:user_id])
+	end
+	#Verifica se a sessão do usuário é válida
+	def user_signed_in?
+		@session[:user_id].present?
+	end
 
 end
